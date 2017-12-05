9272c33c187000f7f8f89bbf34a8a33d39673ca6
Kimberly Wijaya <kcwijaya@stanford.edu>
2017-12-05 02:43:45 -0800

    Update state

--- DeepLearningFlappyBird-master/qlearning.py
+++ DeepLearningFlappyBird-master/qlearning.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("game/")
-import flappy_bird_utils
+import flappy_bird_utils as utils
 import wrapped_flappy_bird as game
 import numpy as np
 
@@ -17,10 +17,10 @@ SCREENHEIGHT = 512
 BASEY = SCREENHEIGHT * 0.79
 
 
-IMAGES, SOUNDS, HITMASKS = flappy_bird_utils.load()
-PIPE_HEIGHT = IMAGES['pipe'][0].get_height();
+IMAGES, SOUNDS, HITMASKS = utils.load()
+# PIPE_HEIGHT = IMAGES['pipe'][0].get_height();
 
-print(PIPE_HEIGHT)
+# print(PIPE_HEIGHT)
 
 game_state = game.GameState()
 do_nothing = np.zeros(2)
@@ -28,21 +28,77 @@ do_nothing[0] = 1
 do_nothing[1] = 0
 
 
-initial_state = (game_state.playery, game_state.playerVelY, (game_state.upperPipes[0]['x'], game_state.upperPipes[0]['y'] + PIPE_HEIGHT), \
-	(-1, -1), (game_state.lowerPipes[0]['x'], game_state.lowerPipes[0]['y'] + PIPE_HEIGHT), (-1, -1))
+initial_state = (game_state.playerx, game_state.playery, game_state.playerVelY, (game_state.upperPipes[0]['x'], game_state.upperPipes[0]['y'] + game.PIPE_HEIGHT), \
+	(-1, -1), (game_state.lowerPipes[0]['x'], game_state.lowerPipes[0]['y'] + game.PIPE_HEIGHT), (-1, -1))
+
+def update_upper_pipes(first_pipe, second_pipe):
+	if (first_pipe[0] < 0):
+		first_pipe = (second_pipe[0]-4, second_pipe[1])
+		second_pipe = (game_state.upperPipes[1]['x'], game_state.upperPipes[1]['y'])
+	elif (second_pipe[0] == -1 and second_pipe[1] == -1):
+		if (game_state.upperPipes[1]['x'] < 400):
+			second_pipe = (game_state.upperPipes[1]['x'], game_state.upperPipes[1]['y'])
+		first_pipe = (first_pipe[0]-4, first_pipe[1])
+	else:
+		first_pipe = (first_pipe[0]-4, first_pipe[1])
+		second_pipe = (second_pipe[0]-4, second_pipe[1])
+	return first_pipe, second_pipe
+
+def update_lower_pipes(first_pipe, second_pipe):
+	if (first_pipe[0] < 0):
+		first_pipe = (second_pipe[0]-4, second_pipe[1])
+		second_pipe = (game_state.lowerPipes[1]['x'], game_state.lowerPipes[1]['y'])
+	elif (second_pipe[0] == -1 and second_pipe[1] == -1):
+		if (game_state.lowerPipes[1]['x'] < 400):
+			second_pipe = (game_state.lowerPipes[1]['x'], game_state.lowerPipes[1]['y'])
+		first_pipe = (first_pipe[0]-4, first_pipe[1])
+	else:
+		first_pipe = (first_pipe[0]-4, first_pipe[1])
+		second_pipe = (second_pipe[0]-4, second_pipe[1])
+	return first_pipe, second_pipe
+
+def calculateScore(state):
+	reward = 0
+	playerMidPos = state[0] + game_state.PLAYER_WIDTH / 2
+	for pipe in [state[3], state[4]]:
+		pipeMidPos = pipe['x'] + game_state.PIPE_WIDTH / 2
+		if pipeMidPos <= playerMidPos < pipeMidPos + 4:
+			reward += 1
+	return reward
 
 def update_state(cur_state, action):
 	new_action = np.zeros(2)
 	if(action[0] == 1): #Dropping
-		new_playery = cur_state[0] + min(self.playerVelY, BASEY - cur_state[0] - PLAYER_HEIGHT)
 		if(new_playery < 0):
 			new_playery = 0
-		new_velocity = min(cur_state[1] + game_state.playerAccY, game_state.playerMaxVelY)
-		
-		new_state = ()
+		new_playery = cur_state[1] + min(game_state.playerVelY, game.BASEY - cur_state[1] - game.PLAYER_HEIGHT)
+		new_velocity = min(cur_state[2] + game_state.playerAccY, game_state.playerMaxVelY)
+	else:
+		new_playery = max(cur_state[1] + min(game_state.playerVelY, game.BASEY - cur_state[1] - game.PLAYER_HEIGHT), 0)
+		new_velocity = game_state.playerFlapAcc
 
+	upper_one, upper_two = update_upper_pipes(cur_state[3], cur_state[4])
+	lower_one, lower_two = update_lower_pipes(cur_state[5], cur_state[6])
+	return (cur_state[0], new_playery, new_velocity, upper_one, upper_two, lower_one, lower_two)
 
-while(True):
-	x_t, r_0, terminal = game_state.frame_step(do_nothing) 
-	#print(game_state.upperPipes[0]['y'] + PIPE_HEIGHT)
-	print(game_state.playerVelY) 
+print ("INITIAL: ", initial_state) 
+state = initial_state
+terminal = False
+count = 0
+
+while(not terminal):
+	do_nothing = np.zeros(2)
+	do_nothing[0] = 1
+	do_nothing[1] = 0
+	
+	# game_state_now = (game_state.playery, game_state.playerVelY, (game_state.upperPipes[0]['x'], game_state.upperPipes[0]['y'] + game.PIPE_HEIGHT), \
+	# (-1, -1) if game_state.upperPipes[1]['x'] >= 400 else (game_state.upperPipes[1]['x'], game_state.upperPipes[1]['y']), (game_state.lowerPipes[0]['x'],\
+	# game_state.lowerPipes[0]['y'] + game.PIPE_HEIGHT), (-1, -1) if game_state.lowerPipes[1]['x'] >= 400 else (game_state.lowerPipes[1]['x'], game_state.lowerPipes[1]['y']))
+
+	# print "GAME AFTER: ", game_state_now, "\n"
+	x_t, r_0, terminal = game_state.frame_step([0,1]) 
+
+	state = update_state(state, [0,1])
+	print "Update ", count, ":", state
+	count += 1
+
